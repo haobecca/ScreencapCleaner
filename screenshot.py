a639762af00efa26c0c3848a935d3ad8478bb379
haobecca <bbeckm@Rebeccas-MacBook-Pro-3.local>
2018-08-27 17:56:32 -0500

    Added comments

--- screenshot.py
+++ screenshot.py
@@ -10,12 +10,12 @@ TRASH = '/~/.Trash'
 EXP_TIME_SEC = 86400.00 
 
 now = dt.datetime.now()
-now_utc = now.timestamp() # Current timestamp in UTC format (seconds)
+now_utc = now.timestamp()  # Current timestamp in UTC format (seconds)
 desktopFiles = os.listdir(path=DESKTOP)
 
 for file in desktopFiles:
     file_os_stat = os.stat(DESKTOP + file)
-    file_ctime = file_os_stat.st_ctime
+    file_ctime = file_os_stat.st_ctime  # Time file was created 
     elapse = now_utc - file_ctime  # Time elapsed since file was created in UTC format (seconds)
 
     # Check if file name starts with 'Screen' and ends with 'png'
